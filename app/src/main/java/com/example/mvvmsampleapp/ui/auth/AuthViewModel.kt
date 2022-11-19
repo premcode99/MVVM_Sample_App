05cb1edb155ea4ea02a5ecb62140db3e1523a9c1
Premnarayan Nagar <Premnarayan.Nagar@in.bosch.com>
2022-11-19 17:42:39 +0530

    LoginActivity.kt data binding with AuthViewModel.kt
    Signed-off-by: Premnarayan Nagar <Premnarayan.Nagar@in.bosch.com>

---
+++ app/src/main/java/com/example/mvvmsampleapp/ui/auth/AuthViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.mvvmsampleapp.ui.auth
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+    var email : String? = null
+    var password : String? = null
+
+    var authListener : AuthListener? = null
+
+    fun onLoginButtonClicked(view : View){
+        authListener?.onStarted()
+
+        if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
+            authListener?.onFailure("Invalid Email and Password")
+            return
+        }
+        authListener?.onSuccess()
+    }
+}
--- app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -2,11 +2,33 @@ package com.example.mvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import com.example.mvvmsampleapp.R
+import com.example.mvvmsampleapp.databinding.ActivityLoginBinding
+import com.example.mvvmsampleapp.util.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        val viewModel  = ViewModelProviders.of(this)[AuthViewModel::class.java]
+
+        binding.viewmodel = viewModel
+        viewModel.authListener = this
+    }
+
+    override fun onStarted() {
+        toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("Login Success")
+    }
+
+    override fun onFailure(message: String) {
+        toast("Login Failure")
     }
 }
